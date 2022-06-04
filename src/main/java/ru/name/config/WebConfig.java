ce1fd14d2b4f7c88e8e8fef8d90dc66d56fb81e6
Dnpypy <denlatypoff@yandex.ru>
2022-06-04 20:27:04 +0300

    the spring methods/component that will search for our views in the WEB-INF folder

--- src/main/java/ru/name/config/WebConfig.java
+++ src/main/java/ru/name/config/WebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @EnableWebMvc
-public abstract class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public ViewResolver getViewResolver() {
@@ -24,7 +25,7 @@ public abstract class WebConfig implements WebMvcConfigurer {
     @Bean
     public FreeMarkerConfigurer getFreeMarkerConfigurer() {
         FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
-        freeMarkerConfigurer.setTemplateLoaderPaths("/", "WEB-INF/views/");
+        freeMarkerConfigurer.setTemplateLoaderPaths("/", "/WEB-INF/views/");
         return freeMarkerConfigurer;
     }
 }
