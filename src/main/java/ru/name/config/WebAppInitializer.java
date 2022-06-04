ad25a32d36f32e27d62c62105eda779271ce1ae9
Dnpypy <denlatypoff@yandex.ru>
2022-06-04 20:32:26 +0300

    Add class WebAppInitializer  that, at the start of our application, creates a context

---
+++ src/main/java/ru/name/config/WebAppInitializer.java
@@ -0,0 +1,11 @@
+package ru.name.config;
+
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class WebAppInitializer extends WebApplicationInitializer {
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+    }
+}
