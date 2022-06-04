dbdbf7565b51a8c337597bc63b18e96acd38322c
Dnpypy <denlatypoff@yandex.ru>
2022-06-04 21:22:54 +0300

    Rename index.html in index.ftl Add GetMapping index template

--- src/main/java/ru/name/controller/UserController.java
+++ src/main/java/ru/name/controller/UserController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class UserController {
 
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
     @GetMapping("/hello")
     public String hello(){
         return "hello";
