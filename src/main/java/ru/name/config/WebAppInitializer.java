789258c2815b2acb7ed2f6acfdb35539b9eb7bcf
Dnpypy <denlatypoff@yandex.ru>
2022-06-04 20:40:27 +0300

    Add register context and new DispatcherServlet

--- src/main/java/ru/name/config/WebAppInitializer.java
+++ src/main/java/ru/name/config/WebAppInitializer.java
@@ -2,10 +2,21 @@ package ru.name.config;
 
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class WebAppInitializer extends WebApplicationInitializer {
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SpringConfig.class, WebConfig.class);
+        context.setServletContext(servletContext);
 
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
     }
 }
