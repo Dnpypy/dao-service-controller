63db29bf62fe16fb8dfc3e874a9221b05c9a2b53
Dnpypy <denlatypoff@yandex.ru>
2022-06-04 21:18:27 +0300

    Update pom.xml, UserController, WebAppInitializer, WebConfig

--- src/main/java/ru/name/config/WebConfig.java
+++ src/main/java/ru/name/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
--- src/main/java/ru/name/controller/UserController.java
+++ src/main/java/ru/name/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/hello")
-    public String getName(){
-        return "hello!";
+    public String hello(){
+        return "hello";
     }
 }
