76aff27bf87ddcec2cb268c95288a0a051be2c48
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 17:17:17 +0300

    Add two @Bean in SpringConfig object JdbcTemplate and DataSource this is the object that will describe the connection to the database

--- src/main/java/ru/name/config/SpringConfig.java
+++ src/main/java/ru/name/config/SpringConfig.java
@@ -4,7 +4,10 @@ package ru.name.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import ru.name.service.TestBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -15,5 +18,17 @@ public class SpringConfig {
     }
 
     @Bean
-    public JdbcTemplate
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/dsc?useSll=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
 }
