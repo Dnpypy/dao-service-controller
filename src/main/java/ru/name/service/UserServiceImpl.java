ad4c1d4020d3ea8febbab1b07a9fb0fb4107cb86
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 20:29:43 +0300

    Add methods in UserServiceImpl

--- src/main/java/ru/name/service/UserServiceImpl.java
+++ src/main/java/ru/name/service/UserServiceImpl.java
@@ -14,6 +14,26 @@ public class UserServiceImpl implements UserService{
     UserDao userDao;
 
     @Override
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    @Override
+    public User getById(int id) {
+        return userDao.getById(id);
+    }
+
+    @Override
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+
+    @Override
+    public void update(User user) {
+        userDao.update(user);
+    }
+
+    @Override
     public List<User> findAll() {
         return userDao.findAll();
     }
