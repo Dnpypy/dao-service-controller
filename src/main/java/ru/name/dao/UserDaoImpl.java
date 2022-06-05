d99303b42010d9f057cc5483c27dc361b5d255aa
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 17:34:46 +0300

    method findAll() should get into the database and get all users

--- src/main/java/ru/name/dao/UserDaoImpl.java
+++ src/main/java/ru/name/dao/UserDaoImpl.java
@@ -1,12 +1,23 @@
 package ru.name.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import ru.name.entity.User;
 
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public UserDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<User> findAll() {
-        return findAll();
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, new UserMapper());
     }
 }
