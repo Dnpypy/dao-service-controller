0ef40daf0d2e24ea7a2a81e98e2f5516b21b1ba6
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 19:10:04 +0300

    expansion of the interface functionality: save, getById, delete, update

--- src/main/java/ru/name/dao/UserDao.java
+++ src/main/java/ru/name/dao/UserDao.java
@@ -6,5 +6,10 @@ import java.util.List;
 
 public interface UserDao {
 
+    void save(User user);
+    User getById(int id);
+    void delete(int id);
+    void update(User user);
     List<User> findAll();
+
 }
