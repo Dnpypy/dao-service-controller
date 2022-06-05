7ae819419eef3ac935f12f5a8dfa527f5bbda4d4
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 17:00:18 +0300

    Add dependency spring-jdbc 4.3.10

--- src/main/java/ru/name/config/SpringConfig.java
+++ src/main/java/ru/name/config/SpringConfig.java
@@ -1,4 +1,19 @@
 package ru.name.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.name.service.TestBean;
+
+@Configuration
 public class SpringConfig {
+
+    @Bean
+    public TestBean getTestBean(){
+        return new TestBean("hello!!!");
+    }
+
+    @Bean
+    public JdbcTemplate
 }
