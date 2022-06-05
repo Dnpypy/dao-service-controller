c01b059c8ee9f827c0e66b4675322c060816cf4a
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 20:22:59 +0300

    Described the methods in the interface UserService

--- src/main/java/ru/name/service/UserService.java
+++ src/main/java/ru/name/service/UserService.java
@@ -6,5 +6,9 @@ import java.util.List;
 
 public interface UserService {
 
+    void save(User user);
+    User getById(int id);
+    void delete(int id);
+    void update(User user);
     List<User> findAll();
 }
