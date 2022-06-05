2169af2c2093537fa1ccacc516f795a656fbd23f
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 17:20:30 +0300

    Implements methods of the UserDao class

---
+++ src/main/java/ru/name/dao/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package ru.name.dao;
+
+import ru.name.entity.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public List<User> findAll() {
+        return findAll();
+    }
+}
