d35c1802001ed6d2cfd05b2f7562389a919b5038
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 20:38:34 +0300

    Adding a link to the get User id request

--- src/main/java/ru/name/controller/UserController.java
+++ src/main/java/ru/name/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.name.service.UserService;
 
@@ -15,19 +17,25 @@ public class UserController {
     public UserService userService;
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
     }
 
     @GetMapping("/users")
-    public String getAllUsers(Model model){
+    public String getAllUsers(Model model) {
         model.addAttribute("users", userService.findAll());
         return "usersList";
+    }
+
+    @GetMapping("/user/{id}")
+    public String getById(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+        return "showUser";
     }
 }
