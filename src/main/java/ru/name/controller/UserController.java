7d5cf82ea531241cecb00e6ef645902b8a31e109
Dnpypy <denlatypoff@yandex.ru>
2022-06-05 18:39:42 +0300

    UserService now we use this service in the controller Let's take a template element that will return all users in the database.

--- src/main/java/ru/name/controller/UserController.java
+++ src/main/java/ru/name/controller/UserController.java
@@ -1,12 +1,17 @@
 package ru.name.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.name.service.UserService;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
+
+    @Autowired
+    public UserService userService;
 
     @GetMapping("/")
     public String index(){
